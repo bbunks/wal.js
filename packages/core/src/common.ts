a414e4604293d4d89e0288bf26789429b6158430
Brenden Bunker <brendenbunker@gmail.com>
2025-01-11 06:44:32 -0800

    reexported the packages

--- packages/core/src/common.ts
+++ packages/core/src/common.ts
@@ -1,6 +1,8 @@
 import { Watcher } from "./Watcher";
 import { ComputeVal } from "./ComputedVal";
 
+export { Watcher, ComputeVal };
+
 if (typeof window !== "undefined") {
   (window as any).Watcher = Watcher;
   (window as any).ComputeVal = ComputeVal;
